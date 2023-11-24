e6a2ba327e2a8a99f5433158f80c5692642aa45e
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-11-24 19:24:12 +0700

    BUILD CRUD WITH AUTHENTICATION

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const userController = require('../controllers/user.controllers');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/',userController.read);
+router.post('/',userController.signup);
+router.post('/signin',userController.signin);
+router.patch('/:id',userController.update);
+router.delete('/:id',userController.destroy);
+router.get('/:id',userController.readbyid);
+
+
 
 module.exports = router;
