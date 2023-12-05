7b1f7bb6a43104604d81a36e07a835b3de2b7ba2
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-12-05 18:50:57 +0700

    CONFIGURE SEEDERS

--- seeders/20231122112853-default-user.js
+++ seeders/20231122112853-default-user.js
@@ -1,28 +1,33 @@
 'use strict';
+const bcrypt = require('bcrypt');
+const { nanoid } = require('nanoid');
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-     await queryInterface.bulkInsert('Users', [
-       {
-          username: 'admin',
-          email: 'admin@gmail.com',
-          fullname: 'admin',
-          password: 'admin',
-          picture: 'admin.png',
-          alamat: 'superuser bio here',
-          telephone: 123456789,
-          role: 'admin',
-          createdAt: new Date(),
-          updatedBy: 0,
-          updatedAt: new Date(),
-          isDeleted: false,
-          isPro: false
-      }       
-    ]);
+  async up(queryInterface, Sequelize) {
+    const hashedPassword = await bcrypt.hash('admin', 10);
+    const userId = nanoid(10); // Generate a unique ID using nanoid
 
+    await queryInterface.bulkInsert('Users', [
+      {
+        id: userId,
+        username: 'admin',
+        email: 'admin@gmail.com',
+        fullname: 'admin',
+        password: hashedPassword,
+        picture: 'admin.png',
+        alamat: 'superuser bio here',
+        telephone: 123456789,
+        role: 'admin',
+        createdAt: new Date(),
+        updatedBy: 0,
+        updatedAt: new Date(),
+        isDeleted: false,
+        isPro: false
+      }
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
