c7a0578b124348759d1981a572ac2563c8827fc5
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-12-05 15:03:37 +0700

    RECONFIGURE MUTLER FORMAT & SIZE

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -14,13 +14,32 @@ const path = require('path');
 const paginate = require('sequelize-paginate');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/users/pictures'); // Destination folder for uploaded files
-    },
-    filename: function (req, file, cb) {
+destination: function (req, file, cb) {
+    cb(null, 'uploads/users/pictures');
+},
+filename: function (req, file, cb) {
     const ext = file.originalname.split('.').pop();
-      cb(null, Date.now() + '-' + file.fieldname + '.' + ext); // Unique filename
-    },
+    cb(null, Date.now() + '-' + file.fieldname + '.' + ext);
+},
+});
+
+const upload = multer({storage: storage,
+fileFilter: function (req, file, cb) {
+    const allowedTypes = ['jpg', 'jpeg', 'png'];
+    const ext = file.originalname.split('.').pop().toLowerCase();
+    if (!allowedTypes.includes(ext)) {
+    const error = new Error('Only JPG and PNG files are allowed');
+    error.code = 'LIMIT_FILE_TYPES';
+    return cb(error, false);
+    }
+    const maxSize = 2 * 1024 * 1024;
+    if (file.size > maxSize) {
+    const error = new Error('File size exceeds the limit (2MB)');
+    error.code = 'LIMIT_FILE_SIZE';
+    return cb(error, false);
+    }
+    cb(null, true);
+},
 });
 
 // CREATE USER
--- routes/recipes.js
+++ routes/recipes.js
@@ -5,16 +5,35 @@ const multer = require('multer');
 
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/recipes/images/'); // Destination folder for uploaded files
-    },
-    filename: function (req, file, cb) {
-      const ext = file.originalname.split('.').pop();
-      cb(null, Date.now() + '-' + file.fieldname + '.' + ext); // Unique filename
-    },
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/recipes/images'); 
+  },
+  filename: function (req, file, cb) {
+    const ext = file.originalname.split('.').pop();
+    cb(null, Date.now() + '-' + file.fieldname + '.' + ext); 
+  },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    const allowedTypes = ['jpg', 'jpeg', 'png'];
+    const ext = file.originalname.split('.').pop().toLowerCase();
+    if (!allowedTypes.includes(ext)) {
+      const error = new Error('Only JPG and PNG files are allowed');
+      error.code = 'LIMIT_FILE_TYPES';
+      return cb(error, false);
+    }
+    const maxSize = 2 * 1024 * 1024; 
+    if (file.size > maxSize) {
+      const error = new Error('File size exceeds the limit (2MB)');
+      error.code = 'LIMIT_FILE_SIZE';
+      return cb(error, false);
+    }
+    cb(null, true);
+  },
+});
+
 
 router.post('/', upload.single('image'), recipeController.createRecipe);
 router.get('/', recipeController.readRecipes);
