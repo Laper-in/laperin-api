432740943744d9fd91bb7ec56c3c9c6d7cb34361
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-12-18 11:34:42 +0700

    ADDING MUTLER SINGLE UPLOAD

---
+++ middlewares/multer.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+
+
+const multerStorage = multer.memoryStorage();
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: function (req, file, cb) {
+    // console.log('log mutlers:', file);
+    const allowedTypes = ["jpg", "jpeg", "png"];
+    const ext = file.originalname.split(".").pop().toLowerCase();
+    if (!allowedTypes.includes(ext)) {
+      const error = new Error("Only JPG and PNG files are allowed");
+      error.code = "LIMIT_FILE_TYPES";
+      return cb(error, false);
+    }
+    const maxSize = 2 * 1024 * 1024;
+    if (file.size > maxSize) {
+      const error = new Error("File size exceeds the limit (2MB)");
+      error.code = "LIMIT_FILE_SIZE";
+      return cb(error, false);
+    }
+    cb(null, true);
+  },
+});
+
+module.exports = { upload };
