56d0400fee66dbd4ae16513bc4e1b0640ecd57a4
Rizaldi Farhan <rizaldifarhan28@gmail.com>
2023-12-18 11:31:03 +0700

    RECONFIGURE BOOKMARKS

--- controllers/bookmark.controllers.js
+++ controllers/bookmark.controllers.js
@@ -1,136 +1,194 @@
-    const { bookmark } = require("../models");
-    const { nanoid } = require("nanoid");
-    const Validator = require("fastest-validator");
-    const v = new Validator();
-    const { auth } = require("../middlewares/auth");
-    const paginate = require("sequelize-paginate");
+const { bookmark , recipe  } = require("../models");
+const { Op } = require("sequelize");
+const {
+  generateAccessToken, clearToken, authData, isUserOwner,isDonationOwner, isAdmin,isUserDeleted,
+} = require('../middlewares/auth');
+const Validator = require("fastest-validator");
+const v = new Validator();
+const paginate = require("sequelize-paginate");
 
-    // CREATE BOOKMARK
-    function createBookmark(req, res, next) {
-    const data = {
-        idBookmark: nanoid(10),
-        idUser: req.body.idUser,
-        idRecipe: req.body.idRecipe,
-        createdAt: new Date(),
-    };
+// CREATE BOOKMARK
+async function createBookmark(req, res, next) {
 
-    // Check if the user ID in the JWT matches the user ID in the request body
-    if (req.user.userid !== data.idUser) {
-        return res
-        .status(403)
-        .json({ message: "Forbidden: User IDs do not match" });
-    }
+  // const userId = req.user.userId; // Extract user ID from JWT
 
-    const schema = {
-        idUser: { type: "string", min: 5, max: 50, optional: true },
-        idRecipe: { type: "string", min: 5, max: 50, optional: true },
-    };
+  const data = {
+    idUser: req.user.userId,
+    idRecipe: req.body.idRecipe,
+    createdAt: new Date(),
+  };
+  if (req.user.userId !== data.idUser) {
+    return res.status(403).json({ message: "Forbidden: User IDs do not match" });
+  }
+  const schema = {
+    idUser: { type: "string", min: 5, max: 50, optional: false },
+    idRecipe: { type: "string", min: 1, max: 50, optional: false },
+  };
+  const validationResult = v.validate(data, schema);
+  if (validationResult !== true) {
+    res.status(400).json({
+      message: "Validation Failed",
+      data: validationResult,
+    });
+  } else {
+    try {
+      const existingBookmark = await bookmark.findOne({
+        where: {
+          idUser: data.idUser,
+          idRecipe: data.idRecipe,
+        },
+      });
 
-    // VALIDASI DATA
-    const validationResult = v.validate(data, schema);
-
-    if (validationResult !== true) {
-        res.status(400).json({
-        message: "Validation Failed",
-        data: validationResult,
+      if (existingBookmark) {
+        return res.status(400).json({
+          message: "Bookmark with the same idUser and idRecipe already exists",
         });
+      }
+      const result = await bookmark.create(data);
+      res.status(201).json({
+        message: "Bookmark Created Successfully",
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Create Bookmark Failed",
+      });
+    }
+  }
+}
+// READ BOOKMARKS
+async function getAllBookmarksByUserId(req, res, next) {
+  const userId = req.user.userId; // Gunakan userId dari JWT
+  const page = parseInt(req.query.page, 10) || 1;
+  const pageSize = parseInt(req.query.pageSize, 10) || 10;
+  try {
+    const { count, rows: bookmarks } = await bookmark.findAndCountAll({
+      where: { idUser: userId },
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    });
+    const idRecipes = bookmarks.map((bookmark) => bookmark.idRecipe);
+    const recipesData = await recipe.findAll({
+      where: {
+        id: {
+          [Op.in]: idRecipes,
+        },
+      },
+    });
+    const bookmarksWithRecipes = bookmarks.map((bookmark) => {
+      const correspondingRecipe = recipesData.find((recipe) => recipe.id === bookmark.idRecipe);
+      return {
+        bookmark: bookmark,
+        recipe: correspondingRecipe,
+      };
+    });
+    const response = {
+      message: "Read bookmarks success",
+      total_count: count,
+      total_pages: Math.ceil(count / pageSize),
+      current_page: page,
+      data: bookmarksWithRecipes,
+    };
+    res.status(200).json(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Read bookmarks failed",
+    });
+  }
+}
+// SEARCH BOOKMARKS BY CATEGORY
+async function searchAllBookmarkByCategory(req, res, next) {
+  const userId = req.user.userId;
+  const page = parseInt(req.query.page, 10) || 1;
+  const pageSize = parseInt(req.query.pageSize, 10) || 10;
+  const category = req.query.category; 
+
+  try {
+    const categoryCondition = category ? {
+      category: {
+        [Op.like]: `%${category}%`,
+      },
+    } : {};
+    const { count, rows: bookmarks } = await bookmark.findAndCountAll({
+      where: { idUser: userId },
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    });
+    let recipesData;
+    if (category) {
+      recipesData = await recipe.findAll({
+        where: {
+          id: {
+            [Op.in]: bookmarks.map((bookmark) => bookmark.idRecipe),
+          },
+          ...categoryCondition,
+        },
+      });
     } else {
-        bookmark.create(data)
-        .then((result) => {
-            res.status(201).json({
-            message: "Bookmark Created Successfully",
-            data: result,
-            });
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json({
-            message: "Create Bookmark Failed",
-            data: err,
-            });
-        });
+      recipesData = await recipe.findAll({
+        where: {
+          id: {
+            [Op.in]: bookmarks.map((bookmark) => bookmark.idRecipe),
+          },
+        },
+      });
     }
-    }
+    const bookmarksWithRecipes = bookmarks.map((bookmark) => {
+      const correspondingRecipe = recipesData.find((recipe) => recipe.id === bookmark.idRecipe);
+      return correspondingRecipe ? {
+        bookmark: bookmark,
+        recipe: correspondingRecipe,
+      } : null;
+    }).filter(Boolean);
 
-    // READ BOOKMARKS
-    function readAllBookmarksByUserId(req, res, next) {
-    const userid = req.params.id;
-
-    // Check if the user ID in the JWT matches the requested user ID
-    if (req.user.userid !== userid) {
-        return res
-        .status(403)
-        .json({ message: "Forbidden: User IDs do not match" });
-    }
-
-    const page = parseInt(req.query.page, 10) || 1;
-    const pageSize = parseInt(req.query.pageSize, 10) || 10;
-
-    bookmark.findAndCountAll({
-        where: { idUser: userid },
-        limit: pageSize,
-        offset: (page - 1) * pageSize,
-    })
-        .then((result) => {
-        const response = {
-            bookmarks: result.rows,
-            total_count: result.count,
-            total_pages: Math.ceil(result.count / pageSize),
-            current_page: page,
-        };
-
-        res.status(200).json(response);
-        })
-        .catch((err) => {
-        console.error(err);
-        res.status(500).json({
-            message: "Read bookmarks failed",
-            data: err,
-        });
-        });
-    }
-
-    // DELETE BOOKMARK
-    function deleteBookmark(req, res, next) {
-    const bookmarkId = req.params.id;
-
-    // Check if req.user is defined and has a userid property
-    if (!req.user || !req.user.userid) {
-        return res.status(401).json({
-        message: "Unauthorized. User information not availaable.",
-        });
-    }
-
-    // Delete the bookmark based on idBookmark
-    bookmark.destroy({
-        where: { idBookmark: bookmarkId, idUser: req.user.userid },
-    })
-        .then((deletedRows) => {
-        if (deletedRows > 0) {
-            res.status(200).json({
-            message: "Bookmark deleted successfully",
-            data: null,
-            });
-        } else {
-            res.status(404).json({
-            message: "Bookmark not found or unauthorized",
-            data: null,
-            });
-        }
-        })
-        .catch((err) => {
-        console.error(err);
-        res.status(500).json({
-            message: "Delete bookmark failed",
-            data: err,
-        });
-        });
-    }
-
-    paginate.paginate(bookmark);
-
-    module.exports = {
-    createBookmark,
-    readAllBookmarksByUserId,
-    deleteBookmark,
+    const response = {
+      message: "Search bookmarks by category success",
+      total_count: bookmarksWithRecipes.length,
+      total_pages: 1,
+      current_page: 1,
+      data: bookmarksWithRecipes,
     };
+
+    res.status(200).json(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Read bookmarks failed",
+    });
+  }
+}
+// DELETE BOOKMARK
+async function deleteBookmark(req, res, next) {
+  const bookmarkId = req.params.id;
+
+  try {
+    const foundBookmark = await bookmark.findOne({
+      where: { idBookmark: bookmarkId, idUser: req.user.userId },
+    });
+
+    if (!foundBookmark) {
+      return res.status(404).json({
+        message: "Bookmark not found or unauthorized",
+      });
+    }
+    await foundBookmark.destroy();
+    res.status(200).json({
+      message: "Bookmark deleted successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Delete bookmark failed",
+    });
+  }
+}
+
+paginate.paginate(bookmark);
+
+module.exports = {
+  createBookmark,
+  getAllBookmarksByUserId,
+  deleteBookmark,
+  searchAllBookmarkByCategory,
+};
--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -1,10 +1,17 @@
     const express = require('express');
     const router = express.Router();
     const bookmarkControllers = require('../controllers/bookmark.controllers');
-    const authMiddleware = require('../middlewares/auth');
+    const {
+        authenticateToken,
+        authenticateRefreshToken,
+        checkBlacklist,
+        isAdmin,
+        isUserOwner,
+      } = require('../middlewares/auth');
 
     // Apply auth middleware to routes that require authentication
-        router.post('/', authMiddleware.auth('user'), bookmarkControllers.createBookmark);
-        router.get('/:id', authMiddleware.auth('user'), bookmarkControllers.readAllBookmarksByUserId);
-        router.delete('/:id',authMiddleware.auth('user'), bookmarkControllers.deleteBookmark);
+        router.post('/', authenticateToken,authenticateRefreshToken,checkBlacklist, bookmarkControllers.createBookmark);
+        router.get('/', authenticateToken,authenticateRefreshToken,checkBlacklist, bookmarkControllers.getAllBookmarksByUserId);
+        router.get('/search', authenticateToken,authenticateRefreshToken,checkBlacklist, bookmarkControllers.searchAllBookmarkByCategory);
+        router.delete('/:id',authenticateToken,authenticateRefreshToken,checkBlacklist, bookmarkControllers.deleteBookmark);
         module.exports = router;
